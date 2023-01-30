8e7b8cfab326d59d99350903a00b88531e4b1419
ingthanhunt <wrcing21@gmail.com>
2023-01-30 21:48:46 +0700

    Create Colors

---
+++ src/utils/Colors.ts
@@ -0,0 +1,13 @@
+const PRIMARY = '#E16D62'; 
+const SECONDARY = '#B8564D'; 
+const TEXT_PRIMARY = '#E7D6D6';
+const WHITE = '#FFFFFF';
+
+const Colors = {
+    PRIMARY,
+    SECONDARY,
+    TEXT_PRIMARY,
+    WHITE,
+};
+
+export default Colors;

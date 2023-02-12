6981ce321863ef7f455737f06132dc0eb59c4839
Pattanan Piyachotinan <70986870+Petoxic@users.noreply.github.com>
2023-02-12 20:49:56 +0700

    Fix conflict with Register-popup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import Registration from '@/components/registration/RegistrationPopup'
-import { Button, ConfigProvider } from 'antd'
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Registration from "@/components/registration/RegistrationPopup";
+import { Button, ConfigProvider } from "antd";
+import LoginButton from "../components/login/LoginButton";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [onRegistration, setOnRegistration] = useState(false);
+    const [onRegistration, setOnRegistration] = useState(false);
 
-  const toggleRegistrationModal = ():void => {
-    setOnRegistration(!onRegistration);
-  };
+    const toggleRegistrationModal = (): void => {
+        setOnRegistration(!onRegistration);
+    };
 
-  return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: '#E16D62',
-        },
-      }}
-    >
-      <Button onClick={()=> toggleRegistrationModal()}/>
-      <Registration
-        toggleRegistrationModal={toggleRegistrationModal}
-        onRegistration={onRegistration}
-      />
-    </ConfigProvider>
-  )
+    return (
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: "#E16D62",
+                },
+            }}
+        >
+            <Button onClick={() => toggleRegistrationModal()} />
+            <Registration
+                toggleRegistrationModal={toggleRegistrationModal}
+                onRegistration={onRegistration}
+            />
+            <LoginButton />
+        </ConfigProvider>
+    );
 }

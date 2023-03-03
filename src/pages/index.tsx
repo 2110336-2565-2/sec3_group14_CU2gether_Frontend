4a8173d079ca23a36ac434a243afe5ba37c29a65
oatRatchanon <88995860+oatRatchanon@users.noreply.github.com>
2023-03-03 17:13:17 +0700

    Uncomment Content in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           onLoginAndRegistrationModal={onLoginAndRegistration}
           toggleLoginAndRegistrationModal={toggleLoginAndRegistrationModal}
         ></LoginAndRegistration>
-        {/* <Content /> */}
+        <Content />
       </HomeContainer>
     </ConfigProvider>
   );

e3ec9c58e899be8c66a71b8820ffbb28904a03b7
Chayakorn Vongbunsin <taecv.work@gmail.com>
2023-03-28 15:10:26 +0700

    Add initial page for web report and event report

---
+++ src/pages/admin/event-reports/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type EventReportsPageProps = {};
+
+const EventReportsPage: React.FC<EventReportsPageProps> = ({}) => {
+  return <></>;
+};
+
+export default EventReportsPage;
---
+++ src/pages/admin/website-reports/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type WebsiteReportsPageProps = {};
+
+const WebsiteReportsPage: React.FC<WebsiteReportsPageProps> = ({}) => {
+  return <></>;
+};
+
+export default WebsiteReportsPage;
